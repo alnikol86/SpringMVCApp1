bfe28df4f154322c2bb3764e06fd4f3e3fbdad76
Alexander Nikolaev <nikolaevairman@gmail.com>
2021-07-03 15:23:30 +0300

    annotation @Controller

--- src/main/java/ru/nikolaev/springcourse/config/MySpringMVCDispatcherServletInitializer.java
+++ src/main/java/ru/nikolaev/springcourse/config/MySpringMVCDispatcherServletInitializer.java
@@ -1,2 +1,20 @@
-package ru.nikolaev.springcourse.config;public class MySpringMVCDispatcherServletInitializer {
+package ru.nikolaev.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
 }
